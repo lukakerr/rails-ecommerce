117220fa75f9c77163f0c7dadbc2d6ac38131cae
lukakerr <lukakerr1@gmail.com>
2018-03-23 19:42:23 +1100

    Update seeds.rb and minor style changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,19 @@
 # Usage: rails db:seed or created alongside the database with rails db:setup
 
 User.delete_all
-User.create! :email => 'test@test.com', :password => 'TestTest', :password_confirmation => 'TestTest', :admin => true, :first_name => 'Test', :last_name => 'Test', :phone => '9999999999'
+User.create! :email => "test@test.com", :password => "TestTest", :password_confirmation => "TestTest", :admin => true, :first_name => "Test", :last_name => "Test", :phone => "9999999999"
+
 Banner.delete_all
-Banner.create! :name => 'Home Page Banner', :user_id => 1
+Banner.create! :name => "Home Page Banner", :user_id => 1
+
 Page.delete_all
-Page.create! :name => 'Contact Us', :content => '<h3 style="text-align: center;">Get in touch below</h3>', :user_id => 1, :contact_form => true
+Page.create! :name => "Contact Us", :content => "<h3 style='text-align: center;'>Get in touch below</h3>", :user_id => 1, :contact_form => true, :published => true, :position => 1
+
+Category.delete_all
+Category.create! :name => "Cushions", :user_id => 1
+
+Product.delete_all
+Product.create! :name => "Product 1", :description => "A great product", :price => 10.50, :old_price => 12.80, :on_sale => true, :sold_out => false, :featured => false, :quantity => 1, :category_id => 1, :user_id => 1
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
